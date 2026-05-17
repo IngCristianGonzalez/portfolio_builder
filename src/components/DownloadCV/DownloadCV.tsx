357a0ebc3c68c1b71789dd703170f5ef5aa7f843
Cristian Alarcon Gonzalez <cristianjussepalarcongonzalez@gmail.com>
2026-05-16 21:04:20 -0500

    complete to download pdf

--- src/components/DownloadCV/DownloadCV.tsx
+++ src/components/DownloadCV/DownloadCV.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useLocale } from "next-intl";
+import { FiDownload } from "react-icons/fi";
 
 export default function DownloadCV() {
 
@@ -17,6 +18,12 @@ export default function DownloadCV() {
       href={cvFile}
       download
       className="
+      group
+
+      inline-flex
+      items-center
+      gap-3
+
       px-6
       py-3
 
@@ -26,22 +33,45 @@ export default function DownloadCV() {
       border-zinc-200
       dark:border-white/10
 
+      bg-white
+      dark:bg-white/[0.03]
+
       text-zinc-800
       dark:text-white
 
+      shadow-sm
+
       hover:border-cyan-500
       hover:text-cyan-500
+
+      hover:shadow-lg
+      hover:shadow-cyan-500/10
+
+      hover:-translate-y-1
 
       transition-all
       duration-300
       "
     >
 
-      {
-        locale === "es"
-          ? "Descargar CV"
-          : "Download CV"
-      }
+      <FiDownload
+        size={18}
+        className="
+        transition-transform
+        duration-300
+        group-hover:translate-y-[2px]
+        "
+      />
+
+      <span>
+
+        {
+          locale === "es"
+            ? "Descargar CV"
+            : "Download CV"
+        }
+
+      </span>
 
     </a>
 
